0d57da750784288e4f5c224236fc1a48ac8445d6
Andrew Gibson <andrew@goofballLogic.com>
2018-05-04 18:48:42 +0100

    babel

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,15 @@ module.exports = {
         filename: `[name].${version}.js`,
         path: path.resolve( __dirname, "dist" )
     
+    },
+    module: {
+    
+        rules: [
+        
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+        
+        ]
+      
     }
 
 };
